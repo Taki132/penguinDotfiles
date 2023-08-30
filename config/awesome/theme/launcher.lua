0384e85a5e67ea62234dbf95960c4fd78f4ba97e
p3nguin-kun <p3nguinkun@proton.me>
2023-08-31 03:26:22 +0700

    update

--- config/awesome/theme/launcher.lua
+++ config/awesome/theme/launcher.lua
@@ -8,7 +8,7 @@ local dpi = beautiful.xresources.apply_dpi
 
 local launcherdisplay = wibox {
 	width = dpi(400),
-	height = dpi(400),
+	height = dpi(430),
 	bg = beautiful.bg_normal,
 	ontop = true,
 	visible = false
