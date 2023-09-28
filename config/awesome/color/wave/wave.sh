e94c69fa6d40b9ac81237de17896a0304d262e44
p3nguin-kun <p3nguinkun@proton.me>
2023-09-28 23:54:59 +0700

    update

--- config/awesome/color/wave/wave.sh
+++ config/awesome/color/wave/wave.sh
@@ -3,16 +3,16 @@
 source $HOME/.config/awesome/color/desktop.sh
 
 # Colors
-BG="#fffaf5"
-FG="#4b4646"
-BL="#4b4646"
-WH="#ebe6e1"
-R="#eb8c8c"
-G="#96e6a5"
-Y="#f0cd96"
-B="#9bb9f0"
-M="#d7a0e6"
-C="#a0e1d2"
+BG="#f0fafa"
+FG="#262626"
+BL="#404040"
+WH="#dce6e6"
+R="#e68383"
+G="#a0e6af"
+Y="#ffcd96"
+B="#83b4e6"
+M="#e1aae1"
+C="#8cd7d2"
 
 # func (change colors in #Colors section)
 term
