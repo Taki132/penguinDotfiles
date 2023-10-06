7faa5b01f1a3ca113e9b99b6e2e97392dbcfbe5e
p3nguin-kun <p3nguinkun@proton.me>
2023-10-06 13:53:21 +0700

    add neorg

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -58,6 +58,27 @@ local plugins = {
             require("plugins.config.bufferline")
         end
     },
+    -- Neorg
+    {
+        "nvim-neorg/neorg",
+        build = ":Neorg sync-parsers",
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            require("neorg").setup {
+                load = {
+                    ["core.defaults"] = {},  -- Loads default behaviour
+                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
+                    ["core.dirman"] = {      -- Manages Neorg workspaces
+                        config = {
+                            workspaces = {
+                                notes = "~/notes",
+                            },
+                        },
+                    },
+                },
+            }
+        end,
+    },
     -- Treesitter
     {
         "nvim-treesitter/nvim-treesitter",
