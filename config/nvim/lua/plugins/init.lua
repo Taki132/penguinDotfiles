83756edff18ff92d4072e2b435dd62c215c16e9c
p3nguin-kun <p3nguinkun@proton.me>
2023-10-06 14:17:10 +0700

    remove neorg bcuz it still unstable

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -58,28 +58,7 @@ local plugins = {
             require("plugins.config.bufferline")
         end
     },
-    -- Neorg
-    {
-        "nvim-neorg/neorg",
-        build = ":Neorg sync-parsers",
-        dependencies = { "nvim-lua/plenary.nvim" },
-        config = function()
-            require("neorg").setup {
-                load = {
-                    ["core.defaults"] = {},  -- Loads default behaviour
-                    ["core.concealer"] = {}, -- Adds pretty icons to your documents
-                    ["core.dirman"] = {      -- Manages Neorg workspaces
-                        config = {
-                            workspaces = {
-                                notes = "~/notes",
-                            },
-                        },
-                    },
-                },
-            }
-        end,
-    },
-    -- Treesitter
+   -- Treesitter
     {
         "nvim-treesitter/nvim-treesitter",
         event = "BufRead",
