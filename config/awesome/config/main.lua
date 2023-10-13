bfc8052d20daa68918550c55fc8a458ec149c24c
p3nguin-kun <p3nguinkun@proton.me>
2023-10-13 22:07:32 +0700

    update:

--- config/awesome/config/main.lua
+++ config/awesome/config/main.lua
@@ -7,20 +7,20 @@ end)
 
 -- Layouts and tag table
 screen.connect_signal("request::desktop_decoration", function(s)
-	tag.connect_signal("request::default_layouts", function()
-    	awful.layout.append_default_layouts({
-			awful.layout.suit.tile,
-			awful.layout.suit.floating,
-			awful.layout.suit.max,
-			awful.layout.suit.spiral.dwindle,
-			awful.layout.suit.fair,
+    tag.connect_signal("request::default_layouts", function()
+        awful.layout.append_default_layouts({
+            awful.layout.suit.tile,
+            awful.layout.suit.floating,
+            awful.layout.suit.max,
+            awful.layout.suit.spiral.dwindle,
+            awful.layout.suit.fair,
 
-    	})
-	end)
+        })
+    end)
 
-if user.jtag then
-    awful.tag({ "一", "二", "三", "四", "五", "六" }, s, awful.layout.layouts[1])
-else
-    awful.tag({ "1", "2", "3", "4", "5", "6" }, s, awful.layout.layouts[1])
-end
+    if user.jtag then
+        awful.tag({ "一", "二", "三", "四", "五", "六" }, s, awful.layout.layouts[1])
+    else
+        awful.tag({ "1", "2", "3", "4", "5", "6" }, s, awful.layout.layouts[1])
+    end
 end)
