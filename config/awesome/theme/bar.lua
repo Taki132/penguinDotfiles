fb7facfe0dc889c974517f0a70444d08c2b212cf
p3nguin-kun <p3nguinkun@proton.me>
2023-10-13 12:41:01 +0700

    update

--- config/awesome/theme/bar.lua
+++ config/awesome/theme/bar.lua
@@ -6,13 +6,13 @@ local dpi = beautiful.xresources.apply_dpi
 -- Menu
 
 local menu = hovercursor(wibox.widget({
-	buttons = {
-		awful.button({}, 1, function()
-			awesome.emit_signal("widget::menu")
-		end),
-	},
-	image = beautiful.awesome,
-	widget = wibox.widget.imagebox,
+  buttons = {
+    awful.button({}, 1, function()
+      awesome.emit_signal("widget::menu")
+    end),
+  },
+  image = beautiful.awesome,
+  widget = wibox.widget.imagebox,
 }))
 
 -- Clock
@@ -22,86 +22,86 @@ local clock = wibox.widget.textclock("%a %I:%M %p")
 -- Screen
 
 screen.connect_signal("request::desktop_decoration", function(s)
-	-- Taglist
+  -- Taglist
 
-	s.taglist = hovercursor(awful.widget.taglist({
-		screen = s,
-		filter = awful.widget.taglist.filter.all,
-		buttons = {
-			awful.button({}, 1, function(t)
-				t:view_only()
-			end),
-		},
-	}))
+  s.taglist = hovercursor(awful.widget.taglist({
+    screen = s,
+    filter = awful.widget.taglist.filter.all,
+    buttons = {
+      awful.button({}, 1, function(t)
+        t:view_only()
+      end),
+    },
+  }))
 
-	-- Tasklist
+  -- Tasklist
 
-	s.tasklist = hovercursor(
-		awful.widget.tasklist({
-		screen = s,
-		filter = awful.widget.tasklist.filter.currenttags,
-		widget_template = {
-			{
-				{
-					id = "text_role",
-					forced_height = dpi(20),
-					widget = wibox.widget.textbox,
-				},
-				valign = "center",
-				halign = "center",
-				widget = wibox.container.place,
-			},
-			id = "background_role",
-			widget = wibox.container.background,
-		},
-		buttons = {
-			awful.button({}, 1, function(c)
-				c:activate({ context = "tasklist", action = "toggle_minimization" })
-			end),
-		},
-	}))
+  s.tasklist = hovercursor(
+    awful.widget.tasklist({
+      screen = s,
+      filter = awful.widget.tasklist.filter.currenttags,
+      widget_template = {
+        {
+          {
+            id = "text_role",
+            forced_height = dpi(20),
+            widget = wibox.widget.textbox,
+          },
+          valign = "center",
+          halign = "center",
+          widget = wibox.container.place,
+        },
+        id = "background_role",
+        widget = wibox.container.background,
+      },
+      buttons = {
+        awful.button({}, 1, function(c)
+          c:activate({ context = "tasklist", action = "toggle_minimization" })
+        end),
+      },
+    }))
 
-	-- Layouts
+  -- Layouts
 
-	s.layouts = hovercursor(awful.widget.layoutbox({
-		screen = s,
-		buttons = {
-			awful.button({}, 1, function()
-				awful.layout.inc(1)
-			end),
-			awful.button({}, 3, function()
-				awful.layout.inc(-1)
-			end),
-		},
-	}))
+  s.layouts = hovercursor(awful.widget.layoutbox({
+    screen = s,
+    buttons = {
+      awful.button({}, 1, function()
+        awful.layout.inc(1)
+      end),
+      awful.button({}, 3, function()
+        awful.layout.inc(-1)
+      end),
+    },
+  }))
 
-	-- Panel
+  -- Panel
 
-	s.wibar = awful.wibar({
-		position = "top",
-		height = dpi(22),
-		bg = beautiful.bg_normal,
-		fg = beautiful.fg_normal,
-		screen = s,
-		widget = {
-			{
-				{
-					menu,
-					s.taglist,
-					s.layouts,
-					spacing = dpi(10),
-					layout = wibox.layout.fixed.horizontal,
-				},
-				s.tasklist,
-				{
-					clock,
-					spacing = dpi(10),
-					layout = wibox.layout.fixed.horizontal,
-				},
-				layout = wibox.layout.align.horizontal,
-			},
-			margins = dpi(2),
-			widget = wibox.container.margin,
-		},
-	})
+  s.wibar = awful.wibar({
+    position = "bottom",
+    height = dpi(22),
+    bg = beautiful.bg_normal,
+    fg = beautiful.fg_normal,
+    screen = s,
+    widget = {
+      {
+        {
+          menu,
+          s.taglist,
+          s.layouts,
+          spacing = dpi(10),
+          layout = wibox.layout.fixed.horizontal,
+        },
+        s.tasklist,
+        {
+          clock,
+          spacing = dpi(10),
+          layout = wibox.layout.fixed.horizontal,
+        },
+        layout = wibox.layout.align.horizontal,
+      },
+      margins = dpi(2),
+      widget = wibox.container.margin,
+    },
+  })
 end)
--- config/awesome/theme/volume.lua
+++ config/awesome/theme/volume.lua
@@ -5,141 +5,141 @@ local beautiful = require("beautiful")
 local dpi = beautiful.xresources.apply_dpi
 
 local volumedisplay = wibox {
-    width = dpi(200),
-    height = dpi(100),
-    bg = beautiful.bg_normal,
-    ontop = true,
-    visible = false
+  width = dpi(200),
+  height = dpi(100),
+  bg = beautiful.bg_normal,
+  ontop = true,
+  visible = false
 }
 
 local percent = wibox.widget {
-    widget = wibox.widget.textbox
+  widget = wibox.widget.textbox
 }
 
 local header = wibox.widget {
+  {
     {
-        {
-            {
-                text = "volume",
-                valign = "center",
-                widget = wibox.widget.textbox
-            },
-            nil,
-            percent,
-            layout = wibox.layout.align.horizontal,
-        },
-        left = dpi(15),
-        right = dpi(15),
-        top = dpi(10),
-        bottom = dpi(10),
-        widget = wibox.container.margin
+      {
+        text = "vol",
+        valign = "center",
+        widget = wibox.widget.textbox
+      },
+      nil,
+      percent,
+      layout = wibox.layout.align.horizontal,
     },
-    bg = beautiful.bg_focus,
-    widget = wibox.container.background
+    left = dpi(15),
+    right = dpi(15),
+    top = dpi(10),
+    bottom = dpi(10),
+    widget = wibox.container.margin
+  },
+  bg = beautiful.bg_focus,
+  widget = wibox.container.background
 }
 
 local icon = wibox.widget {
-    font = user.fonticon,
-    valign = "center",
-    widget = wibox.widget.textbox
+  font = user.fonticon,
+  valign = "center",
+  widget = wibox.widget.textbox
 }
 
 local bar = wibox.widget {
-    shape = gears.shape.rounded_rect,
-    bar_shape = gears.shape.rounded_rect,
-    color = beautiful.fg_normal,
-    background_color = beautiful.bg_focus,
-    max_value = 100,
-    value = 0,
-    widget = wibox.widget.progressbar
+  shape = gears.shape.rounded_rect,
+  bar_shape = gears.shape.rounded_rect,
+  color = beautiful.fg_normal,
+  background_color = beautiful.bg_focus,
+  max_value = 100,
+  value = 0,
+  widget = wibox.widget.progressbar
 }
 
 local timer = gears.timer {
-    timeout = 2,
-    single_shot = true,
-    callback = function()
-        volumedisplay.visible = false
-    end
+  timeout = 2,
+  single_shot = true,
+  callback = function()
+    volumedisplay.visible = false
+  end
 }
 
 volumedisplay:setup {
-    header,
+  header,
+  {
     {
-        {
-            {
-                icon,
-                right = dpi(15),
-                widget = wibox.container.margin
-            },
-            nil,
-            {
-                bar,
-                top = dpi(27),
-                bottom = dpi(27),
-                widget = wibox.container.margin
-            },
-            layout = wibox.layout.align.horizontal
-        },
-        left = dpi(15),
+      {
+        icon,
         right = dpi(15),
         widget = wibox.container.margin
+      },
+      nil,
+      {
+        bar,
+        top = dpi(27),
+        bottom = dpi(27),
+        widget = wibox.container.margin
+      },
+      layout = wibox.layout.align.horizontal
     },
-    layout = wibox.layout.align.vertical
+    left = dpi(15),
+    right = dpi(15),
+    widget = wibox.container.margin
+  },
+  layout = wibox.layout.align.vertical
 }
 
 awesome.connect_signal("signal::volume", function(volume, mute)
-    if mute then
-        percent.text = "Mute"
-        icon.text = ""
-    else
-        percent.text = tostring(volume) .. "%"
-        bar.value = volume
-        if volume > 100 then
-            icon.text = ""
-        elseif volume >= 50 then
-            icon.text = ""
-        elseif volume >= 25 then
-            icon.text = ""
-        elseif volume > 0 then
-            icon.text = ""
-        elseif volume == 0 then
-            icon.text = ""
-        end
+  if mute then
+    percent.text = "Mute"
+    icon.text = ""
+  else
+    percent.text = tostring(volume) .. "%"
+    bar.value = volume
+    if volume > 100 then
+      icon.text = ""
+    elseif volume >= 50 then
+      icon.text = ""
+    elseif volume >= 25 then
+      icon.text = ""
+    elseif volume > 0 then
+      icon.text = ""
+    elseif volume == 0 then
+      icon.text = ""
     end
+  end
 end)
 
 awesome.connect_signal("widget::volume", function()
-    awesome.emit_signal("widget::brightness:hide")
+  awesome.emit_signal("widget::brightness:hide")
 
-    timer:again()
+  timer:again()
 
-    if client.focus and client.focus.fullscreen == true then
-        awful.placement.top_right(
-            volumedisplay,
-            {
-                margins = {
-                    top = dpi(10),
-                    right = dpi(10)
-                },
-                parent = awful.screen.focused()
-            }
-        )
-    else
-        awful.placement.top_right(
-            volumedisplay,
-            {
-                margins = {
-                    top = dpi(34),
-                    right = dpi(10)
-                },
-                parent = awful.screen.focused()
-            }
-        )
-    end
+  if client.focus and client.focus.fullscreen == true then
+    awful.placement.bottom_right(
+      volumedisplay,
+      {
+        margins = {
+          bottom = dpi(10),
+          right = dpi(10)
+        },
+        parent = awful.screen.focused()
+      }
+    )
+  else
+    awful.placement.bottom_right(
+      volumedisplay,
+      {
+        margins = {
+          bottom = dpi(34),
+          right = dpi(10)
+        },
+        parent = awful.screen.focused()
+      }
+    )
+  end
 
-    volumedisplay.visible = true
+  volumedisplay.visible = true
 end)
 
 awesome.connect_signal("widget::volume:hide", function()
-    volumedisplay.visible = false
+  volumedisplay.visible = false
 end)
